4bd0dc6f569e532b0dfc107f037fd53f3f94a1cc
D.R Chibuike <66283877+drchibs@users.noreply.github.com>
2021-03-02 17:45:42 +0100

    Create spotify.js

---
+++ spotify.js
@@ -0,0 +1,52 @@
+//Endpoint
+{{URL}}api/v1/chips
+
+//Call
+{
+    "url": "https://open.spotify.com/playlist/36KQJr944ctfcidlNniItt?si=olh7jn88RJisJkgj-20xsw&nd=1"
+}
+
+//Response
+{
+    "status": "success",
+    "data": {
+        "chip": {
+            "favorite": false,
+            "created_at": "2021-03-02T16:30:37.570Z",
+            "modified_at": "2021-03-02T16:30:37.570Z",
+            "clicks": 0,
+            "metadata": [
+                {
+                    "url": "https://open.spotify.com/playlist/36KQJr944ctfcidlNniItt?si=olh7jn88RJisJkgj-20xsw&nd=1",
+                    "title": "UK Christian 🙏🏽🔥",
+                    "siteName": "Spotify",
+                    "description": "Listen on Spotify: ",
+                    "mediaType": "music.playlist",
+                    "contentType": "text/html; charset=utf-8",
+                    "images": [
+                        "https://mosaic.scdn.co/640/ab67616d0000b27314e0c57c117981345453db80ab67616d0000b2736fc6e2770aaa25842b6251b0ab67616d0000b2737e8df365405a3521661e048dab67616d0000b2738375844de8e3eb6def6fab20"
+                    ],
+                    "videos": [],
+                    "favicons": [
+                        "https://open.scdn.co/cdn/images/favicon.0c211e2e.png"
+                    ]
+                }
+            ],
+            "opengraph": [
+                {
+                    "og:title": "UK Christian 🙏🏽🔥",
+                    "og:url": "https://open.spotify.com/playlist/36KQJr944ctfcidlNniItt",
+                    "og:type": "music.playlist",
+                    "og:restrictions:country:allowed": "CW"
+                }
+            ],
+            "twittercard": [
+                {}
+            ],
+            "_id": "603e696070987149c489618f",
+            "url": "https://open.spotify.com/playlist/36KQJr944ctfcidlNniItt?si=olh7jn88RJisJkgj-20xsw&nd=1",
+            "owner_id": "5ff4cd8d84359f2da0b61496",
+            "__v": 0
+        }
+    }
+}
