000cb5de76b4f33549668ed6a60b55cd755164d3
D.R Chibuike <66283877+drchibs@users.noreply.github.com>
2021-03-02 17:49:19 +0100

    Create fountainstream.js

---
+++ fountainstream.js
@@ -0,0 +1,47 @@
+//Endpoint
+{{URL}}api/v1/chips
+
+//Call
+{
+    "url": "https://fountainstream.com/series/11"
+}
+
+//Response
+{
+    "status": "success",
+    "data": {
+        "chip": {
+            "favorite": false,
+            "created_at": "2021-03-02T16:30:37.570Z",
+            "modified_at": "2021-03-02T16:30:37.570Z",
+            "clicks": 0,
+            "metadata": [
+                {
+                    "url": "https://fountainstream.com/login",
+                    "title": "Fountain Stream\n",
+                    "description": "Unlimited Spiritual Meal At Your Finger Tips",
+                    "mediaType": "website",
+                    "contentType": "text/html; charset=UTF-8",
+                    "images": [
+                        "https://fountainstream.com/frontend/images/logo.png",
+                        "https://fountainstream.com/frontend/images/bg.png"
+                    ],
+                    "videos": [],
+                    "favicons": [
+                        "https://fountainstream.com/frontend/images/favicon2.png"
+                    ]
+                }
+            ],
+            "opengraph": [
+                {}
+            ],
+            "twittercard": [
+                {}
+            ],
+            "_id": "603e6c4870987149c4896190",
+            "url": "https://fountainstream.com/series/11",
+            "owner_id": "5ff4cd8d84359f2da0b61496",
+            "__v": 0
+        }
+    }
+}
